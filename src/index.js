7207b801670f9a9cf17e1747e19604ab1fe369a7
Raphael Suter <raphael.werner.suter@gmail.com>
2021-01-11 13:40:38 +0100

    Aufgabe 4

--- src/index.js
+++ src/index.js
@@ -1,9 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Form, ListGroup } from "react-bootstrap";
 
 const App = () => {
-  return <></>;
+  const [value, setValue] = useState("");
+  const [stationboard, setStationboard] = useState([]);
+
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return `${date.getHours()}:${date.getMinutes()}`;
+  };
+
+  useEffect(() => {
+    fetch(`http://transport.opendata.ch/v1/stationboard?id=${value}&limit=10`)
+      .then((response) => response.json())
+      .then(({ stationboard }) => {
+        setStationboard(stationboard);
+      });
+  }, [value]);
+
+  return (
+    <Container className="my-5">
+      <h1>Stationboard</h1>
+      <Form.Control
+        className="my-4"
+        type="text"
+        placeholder="Ort"
+        value={value}
+        onChange={(evt) => setValue(evt.target.value)}
+      />
+      <ListGroup>
+        {stationboard && stationboard.length > 0 ? (
+          stationboard.map(
+            ({ name, to, stop: { platform, departureTimestamp } }, index) => (
+              <ListGroup.Item key={index} className="d-flex flex-column">
+                <div className="d-flex justify-content-between">
+                  <p className="mb-0">{name}</p>
+                  <p className="mb-0 font-weight-bold">Gleis {platform}</p>
+                </div>
+                <div className="d-flex justify-content-between">
+                  <p className="mb-0 font-weight-bold">{to}</p>
+                  <p className="mb-0">{formatTimestamp(departureTimestamp)}</p>
+                </div>
+              </ListGroup.Item>
+            )
+          )
+        ) : (
+          <p>Dieser Ort wurde nicht gefunden.</p>
+        )}
+      </ListGroup>
+    </Container>
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
